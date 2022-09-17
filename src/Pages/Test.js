27e3d444558d4cd6bf0d44437c0ff9d2c36fb9c4
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-09-17 19:06:21 +0100

    test page is fully responsive

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -9,13 +9,13 @@ import "../css/test.css"
 const Test = () => {
   return (
     <div className='mainTestPage'>
-      <div className='school-name mb-0 py-1'>
+      <div className='school-name mb-0 py-1 px-4'>
         <h3 className='text-center m-0'>Unilag Post-UTME practice questions</h3>
       </div>
 
       <div className='timer-container py-3 mt-0'>
-        <div className='col-11 m-auto d-flex align-items-center justify-content-between'>
-          <div className='info-div d-flex align-items-center'>
+        <div className='col-11 timer-container-div m-auto d-flex justify-content-between'>
+          <div className='info-div d-flex align-items-center justify-content-start'>
             <IoPersonCircleOutline className='display-4 person-icon me-3' />
             <span className='person-name'>Egondu</span>
           </div>
@@ -25,65 +25,47 @@ const Test = () => {
               <GiAlarmClock className='display-5 me-2' />
               <span className='time'>00:40:00</span>
             </div>
-            <button className='btn px-4 py-3'>Submit</button>
+            <button className='btn btn-danger px-sm-4 px-3 py-sm-3 py-2'>Submit</button>
           </div>
         </div>
       </div>
-    </div>
 
-    /*<div className='mainTestPage'>
-      {/*<nav className="navbar shadow-sm">
-        <div className="container-fluid">
-          <a className='navbar-brand' href='fdd'>
-            <img src={logo} alt="" width="70" height="70" />
-          </a>
-        </div>
-  </nav >
-      <nav className="navbar shadow-sm rounded rounded-pill fixed-top">
-        <div className="container-fluid align-items-center px-5 py-3">
-          <p className='p-0 m-0 fs-5'>Time left: <span>40:00</span></p>
-          <button className='btn btn-outline-danger border border-3 border-danger px-5 text-uppercase py-2'>End Test</button>
-        </div>
-      </nav >
-      <main className='mainTestDiv shadow'>
-        {/*<h1 className='text-center'>University of Lagos post-utme exam practice</h1>
-        <div className='row mainTestContaier'>
-          <div className='col-5 d-xl-flex d-none bg-white flex-column align-items-center justify-content-center'>
-            <img src={schoollogo} alt="" />
-          </div>
-          <div className='col-xl-7 col-12'>
-            <div className='questionsDiv mt-5 pt-5 px-4'>
-              <div>
-                <p className='testQuestion'>Who is the president of Nigeria?</p>
-                <div className='testOptionsDiv ms-5 mt-5'>
-                  <div className='testOption'>
-                    <input type={"radio"} id="option1" name='options' className='me-5' />
-                    <label htmlFor='option1' className='ms-4'>Muhammed Buhari</label>
-                  </div>
-                  <div className='testOption'>
-                    <input type={"radio"} id="option1" name='options' className='me-5' />
-                    <label htmlFor='option1' className='ms-4'>Muhammed Buhari</label>
-                  </div>
-                  <div className='testOption'>
-                    <input type={"radio"} id="option1" name='options' className='me-5' />
-                    <label htmlFor='option1' className='ms-4'>Muhammed Buhari</label>
-                  </div>
-                  <div className='testOption'>
-                    <input type={"radio"} id="option1" name='options' className='me-5' />
-                    <label htmlFor='option1' className='ms-4'>Muhammed Buhari</label>
-                  </div>
-                </div>
+      <main>
+        <div className='col-11 m-auto mt-5'>
+          <h4 className='mb-5'>Question  1 / 30</h4>
+          <div>
+            <p className='testQuestion'>Who is the president of Nigeria?</p>
+            <div className='testOptionsDiv ms-md-5 ms-3 mt-4'>
+              <div className='testOption'>
+                <span className='alphabetOption'>A</span>
+                <input type={"radio"} id="option1" name='options' className='' />
+                <label htmlFor='option1' className='ms-2'>Muhammed Buhari</label>
               </div>
-              <div className='d-flex align-items-center justify-content-between pe-4 mt-5 pt-5'>
-                <button className='btn directionBtn py-3 px-5'>Prev</button>
-                <button className='btn directionBtn py-3 px-5'>Next</button>
+              <div className='testOption'>
+                <span className='alphabetOption'>B</span>
+                <input type={"radio"} id="option1" name='options' className='' />
+                <label htmlFor='option1' className='ms-2'>Muhammed Buhari</label>
               </div>
-              <p className='text-center mt-5 fw-bold fs-5'>Question 1/30</p>
+              <div className='testOption'>
+                <span className='alphabetOption'>C</span>
+                <input type={"radio"} id="option1" name='options' className='' />
+                <label htmlFor='option1' className='ms-2'>Muhammed Buhari</label>
+              </div>
+              <div className='testOption'>
+                <span className='alphabetOption'>D</span>
+                <input type={"radio"} id="option1" name='options' className='' />
+                <label htmlFor='option1' className='ms-2'>Muhammed Buhari</label>
+              </div>
             </div>
+          </div>
+
+          <div className='d-flex align-items-center justify-content-between pe-4 mt-5 pt-5'>
+            <button className='btn directionBtn px-sm-5 px-4 py-sm-3 py-2'>Prev</button>
+            <button className='btn directionBtn px-sm-5 px-4 py-sm-3 py-2'>Next</button>
           </div>
         </div>
       </main>
-  </div >*/
+    </div>
   )
 }
 
