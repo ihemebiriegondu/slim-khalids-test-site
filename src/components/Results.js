c82351d646ecd71fbfb0e25ffa295c08dfc8c3c7
ihemebiriegondu <ihemebiriegondu@gmail.com>
2022-09-20 22:35:01 +0100

    close window

--- src/components/Results.js
+++ src/components/Results.js
@@ -9,6 +9,10 @@ export default class Results extends Component {
         let participantName = localStorage.getItem("participantName");
         //let newTotalScore = localStorage.getItem("UserTotalScore");
 
+        const closeWindow = () => {
+            window.close();
+        }
+
         return (
             <div className='mainResultDiv'>
                 <img src={bigLogo} alt="" className='mainLoginImg' />
@@ -20,7 +24,7 @@ export default class Results extends Component {
                             <p className='fs-5'>Score: <span id='score'></span> out of 30</p>
 
                             <div className='text-center mt-5 retake-btn'>
-                                <a href='https://slim-khalid-practice.netlify.app/instructions?' onClick={window.close()} className='btn text-white rounded rounded-2 px-5 py-3'>Retake</a>
+                                <a href='https://slim-khalid-practice.netlify.app/instructions?' onClick={(e) => {closeWindow()}} className='btn text-white rounded rounded-2 px-5 py-3'>Retake</a>
                             </div>
                         </div>
                     </div>
